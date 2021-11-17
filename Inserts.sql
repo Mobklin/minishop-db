54dad73bc274d71b6dd6f71eadb1c9d6bc0e9e58
Kimberly <kimberly.brickmann@BK.local>
2021-11-17 09:16:10 +0100

    Monitor ergänzt

--- Inserts.sql
+++ Inserts.sql
@@ -11,7 +11,7 @@ INSERT INTO Artikel (ArtikelNr,Bezeichnung,Preis) VALUES
 (99700,'Krawatte',30),
 (99123,'Wecker',14.49),
 (99124,'Taschenkalender',5.99)
-(20625),'Monitor',;
+(20625,'Monitor', 14.99);
 
 
 INSERT INTO Bestellung (KundenNr,Datum) VALUES
